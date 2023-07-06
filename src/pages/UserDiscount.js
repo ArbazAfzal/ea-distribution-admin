e75c61644fad2e61d75f0a916610ec2cc5d10746
SMAbidZaidi <127386175+SMAbidZaidi@users.noreply.github.com>
2023-07-06 15:10:30 +0500

    Discount page

--- src/pages/UserDiscount.js
+++ src/pages/UserDiscount.js
@@ -1,92 +1,119 @@
-import { Input } from '@windmill/react-ui';
-import Error from 'components/form/Error';
-import LabelArea from 'components/form/LabelArea';
-import useAsync from 'hooks/useAsync';
-import { t } from 'i18next';
-import Multiselect from 'multiselect-react-dropdown';
-import React, { useState } from 'react'
-import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
-import CustomerServices from 'services/CustomerServices';
+import { Input } from "@windmill/react-ui";
+import LabelArea from "components/form/LabelArea";
+import { SidebarContext } from "context/SidebarContext";
+import useAsync from "hooks/useAsync";
+import { t } from "i18next";
+import Multiselect from "multiselect-react-dropdown";
+import React, { useContext, useState } from "react";
+import CustomerServices from "services/CustomerServices";
+import ProductServices from "services/ProductServices";
 
 const UserDiscount = () => {
-
-//   const { id } = useParams();
-
-
-    const { data } = useAsync(CustomerServices.getAllCustomers);
-    console.log("🚀 ~ file: UserDiscount.js:18 ~ UserDiscount ~ data:", data)
-    const options = data.map((item) => {
-        const obj = {
-          // _id: item.email,
-          name: item.email,
-        };
-        return obj;
-      });
-    const [email, setEmail] = useState([]);
+  const [email, setEmail] = useState([]);
   const [disPrice, setDisPrice] = useState(0);
+  const [name, setName] = useState([]);
+  const [cusId, setCusId] = useState([]);
+  const [proId, setProId] = useState([]);
+
+  const res = useAsync(CustomerServices.getAllCustomers);
+  const optionsForCustomer =
+    res.data?.map(({email,_id}) => {
+      const obj = {
+        name: email,
+      };
+      return obj;
+    }) ?? [];
+
+  const {
+    toggleDrawer,
+    lang,
+    currentPage,
+    handleChangePage,
+    searchText,
+    category,
+    setCategory,
+    searchRef,
+    handleSubmitForAll,
+    sortedField,
+    setSortedField,
+    limitData,
+  } = useContext(SidebarContext);
+
+  const { data, loading } = useAsync(() =>
+    ProductServices.getAllProducts({
+      page: currentPage,
+      limit: limitData,
+      category: category,
+      title: searchText,
+      price: sortedField,
+    })
+  );
+  const products = data?.products;
+  const optionsForProducts = Array.isArray(products)
+    ? products.map(({ _id, slug }) => {
+        const object = {
+          namee: slug,
+        };
+        return object;
+      })
+    : [];
+
   return (
-   <>
-     <>
-                  <div className="grid grid-cols-6 gap-3 md:gap-5 xl:gap-6 lg:gap-6 mb-6">
-                    <LabelArea label={t("Customer Email")} />
-                    <div className="col-span-8 sm:col-span-4">
-                      <Multiselect
-                        displayValue="name"
-                        isObject={true}
-                        singleSelect={false} // Enable multiple selections
-                        // ref={resetRefTwo}
-                        hidePlaceholder={false}
-                        onKeyPressFn={function noRefCheck() {}}
-                        onRemove={function noRefCheck() {}}
-                        onSearch={function noRefCheck() {}}
-                        onSelect={(e) => setEmail(e)}
-                        selectedValues={email}
-                        options={options}
-                        placeholder={"Customer Email"}
-                      ></Multiselect>
-                    </div>
-                  </div>
-                  <div className="grid grid-cols-6 gap-3 md:gap-5 xl:gap-6 lg:gap-6 mb-6">
-                    <LabelArea label={t("Product Name")} />
-                    <div className="col-span-8 sm:col-span-4">
-                      <Multiselect
-                        displayValue="name"
-                        isObject={true}
-                        singleSelect={false} // Enable multiple selections
-                        // ref={resetRefTwo}
-                        hidePlaceholder={false}
-                        onKeyPressFn={function noRefCheck() {}}
-                        onRemove={function noRefCheck() {}}
-                        onSearch={function noRefCheck() {}}
-                        onSelect={(e) => setEmail(e)}
-                        selectedValues={email}
-                        options={options}
-                        placeholder={"Emter product name"}
-                      ></Multiselect>
-                    </div>
-                  </div>
+    <>
+      <div className="grid grid-cols-6 gap-3 md:gap-5 xl:gap-6 lg:gap-6 mb-6">
+        <LabelArea label={t("Customer Email")} />
+        <div className="col-span-8 sm:col-span-4">
+          <Multiselect
+            displayValue="name"
+            isObject={true}
+            singleSelect={false} // Enable multiple selections
+            // ref={resetRefTwo}
+            hidePlaceholder={false}
+            onKeyPressFn={function noRefCheck() {}}
+            onRemove={function noRefCheck() {}}
+            onSearch={function noRefCheck() {}}
+            onSelect={(e) => setEmail(e)}
+            selectedValues={email}
+            options={optionsForCustomer}
+            placeholder={"Customer Email"}
+          ></Multiselect>
+        </div>
+      </div>
+      <div className="grid grid-cols-6 gap-3 md:gap-5 xl:gap-6 lg:gap-6 mb-6">
+        <LabelArea label={t("Product Name")} />
+        <div className="col-span-8 sm:col-span-4">
+          <Multiselect
+            displayValue="namee"
+            isObject={true}
+            singleSelect={false} // Enable multiple selections
+            // ref={resetRefTwo}
+            hidePlaceholder={false}
+            onKeyPressFn={function noRefCheck() {}}
+            onRemove={function noRefCheck() {}}
+            onSearch={function noRefCheck() {}}
+            onSelect={(e) => setName(e)}
+            selectedValues={name}
+            options={optionsForProducts}
+            placeholder={"Enter product name"}
+          ></Multiselect>
+        </div>
+      </div>
 
-                  <div className="grid grid-cols-6 gap-3 md:gap-5 xl:gap-6 lg:gap-6 mb-6">
-                    <LabelArea label={t("Discount")} />
-                    <div className="col-span-8 sm:col-span-4">
-                      <Input
-                        type="number"
-                        placeholder="Enter discount price"
-                        onChange={(e) => setDisPrice(e.target.value)}
-                        defaultValue={0.0}
-                        className="border h-12 text-sm focus:outline-none block w-full bg-gray-100 dark:bg-white border-transparent focus:bg-white"
-                      />
+      <div className="grid grid-cols-6 gap-3 md:gap-5 xl:gap-6 lg:gap-6 mb-6">
+        <LabelArea label={t("Discount")} />
+        <div className="col-span-8 sm:col-span-4">
+          <Input
+            type="number"
+            placeholder="Enter discount price"
+            onChange={(e) => setDisPrice(e.target.value)}
+            defaultValue={0.0}
+            className="border h-12 text-sm focus:outline-none block w-full bg-gray-100 dark:bg-white border-transparent focus:bg-white"
+          />
+          {/* <Error errorName={errors?.dis} /> */}
+        </div>
+      </div>
+    </>
+  );
+};
 
-                    
-                      {/* <Error errorName={errors?.dis} /> */}
-                    </div>
-                  </div>
-                </>
-
-   
-   
-   </>
-  )
-}
-
-export default UserDiscount
+export default UserDiscount;
